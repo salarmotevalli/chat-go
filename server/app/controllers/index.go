d1d68185c7a4f4869891f580400a527cdfffe1e2
salarmotevalli <salar.mo.ro@gmail.com>
2023-07-09 21:30:44 +0330

    add get message end point

--- server/app/controllers/index.go
+++ server/app/controllers/index.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"chat/app/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"net/http"
 )
@@ -30,6 +31,41 @@ func AddMessage(ctx *gin.Context) {
 
 }
 
+type getMessagePayload struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
 func GetMessage(ctx *gin.Context) {
+	var request getMessagePayload
+
+	// get the request body and bind it to the User object
+	if err := ctx.Bind(&request); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	messageModel := models.MessageModel()
+	from, err := primitive.ObjectIDFromHex(request.From)
+	to, err := primitive.ObjectIDFromHex(request.To)
+	if err != nil {
+		log.Println(err.Error())
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	users := []primitive.ObjectID{
+		from,
+		to,
+	}
+
+	messages, err := messageModel.WhereEq("users", users)
+	if err != nil {
+		log.Println(err.Error())
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, messages)
 
 }

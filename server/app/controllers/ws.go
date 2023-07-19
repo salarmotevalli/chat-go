cb7a0243cafec0c49453e1f298d651eeed5738ed
salarmotevalli <salar.mo.ro@gmail.com>
2023-07-19 15:13:27 +0330

    refactor main.go

--- server/app/controllers/ws.go
+++ server/app/controllers/ws.go
@@ -1,2 +1,40 @@
 package controllers
 
+import (
+	"log"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func SetupSocket(socket *socketio.Server) {
+	socket.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		log.Println("connected:", s.ID())
+		return nil
+	})
+
+	socket.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		log.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+
+	socket.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+
+	socket.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+
+	socket.OnError("/", func(s socketio.Conn, e error) {
+		log.Println("meet error:", e)
+	})
+
+	socket.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println("closed", reason)
+	})
+}
--- server/app/routes/routs.go
+++ server/app/routes/routs.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,39 +10,7 @@ import (
 )
 
 func Setup(engine *gin.Engine, socket *socketio.Server)  {
-	
-	socket.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		log.Println("connected:", s.ID())
-		return nil
-	})
-
-	socket.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		log.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-
-	socket.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-
-	socket.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-
-	socket.OnError("/", func(s socketio.Conn, e error) {
-		log.Println("meet error:", e)
-	})
-
-	socket.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		log.Println("closed", reason)
-	})
-
-
+	controllers.SetupSocket(socket)
 	
 	engine.Use(corsMiddleware("http://localhost:3000"))
 	

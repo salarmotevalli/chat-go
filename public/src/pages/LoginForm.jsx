670ba69709c9ab7a454504956072fa1d2fdb2905
morii008 <mori.238sadeghi@gmail.com>
2023-07-28 14:08:04 +0330

    Implement login form validation

--- public/src/pages/LoginForm.jsx
+++ public/src/pages/LoginForm.jsx
@@ -1,25 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import style from "./Form.module.css";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { loginRoute } from "../utils/APIRoutes";
+import { useNavigate } from "react-router-dom";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(4, { message: "The username must be 4 characters or more" })
+    .max(10, { message: "The username must be 10 characters or less" })
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "The username must contain only letters, numbers and underscore (_)"
+    ),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have more than 8 characters"),
+});
 
 const LoginForm = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
+    }
+  }, []);
+
+  const onSubmit = async (data) => {
+    try {
+      const values = schema.parse(data);
+      const { username, password } = values;
+      const { data: response } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (response.status === false) {
+        setErrorMessage(response.msg);
+      }
+      if (response.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(response.user)
+        );
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
   return (
     <>
       <div className="row justify-content-center">
         <div className="col-12 col-md-10">
-          <form className="form-control bg-transparent d-flex justify-content-center flex-column border-0">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="form-control bg-transparent d-flex justify-content-center flex-column border-0"
+          >
+            {errorMessage && <p className="text-danger">{errorMessage}</p>}
             <input
+              {...register("username")}
               type="text"
               placeholder="Username"
               name="username"
               className={`w-100 rounded bg-transparent mb-3 text-white ${style.input}`}
             />
+            {errors.username && (
+              <p className="text-danger">{errors.username.message}</p>
+            )}
             <input
+              {...register("password")}
               type="password"
               placeholder="Password"
               name="password"
               className={`w-100 rounded bg-transparent my-2 my-sm-3 text-white ${style.input}`}
             />
+            {errors.password && (
+              <p className="text-danger">{errors.password.message}</p>
+            )}
             <button
               type="submit"
               className={`border-0 rounded text-uppercase fw-bold text-white my-3 my-sm-4 ${style.login_btn}`}
@@ -27,7 +97,7 @@ const LoginForm = () => {
               Login
             </button>
             <span
-              className={`mb-4 text-center text-white text-uppercase ${style.link_text}`}
+              className={`mb-5 text-center text-white text-uppercase ${style.link_text}`}
             >
               Don't have an account ?{" "}
               <Link
--- public/src/pages/LoginFormContainer.jsx
+++ public/src/pages/LoginFormContainer.jsx
@@ -7,7 +7,7 @@ const LoginFormContainer = () => {
   return (
     <div className="row justify-content-center mt-3">
       <div
-        className={`col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 rounded-5 mt-5 p-4 ${style.form_bg}`}
+        className={`col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 rounded-5 mt-5 ${style.form_bg}`}
       >
         <div className="brand d-flex justify-content-center align-items-center flex-column flex-sm-row mt-0 mb-2 mt-sm-5 mb-sm-4">
           <img src={logo} alt="logo" className={` ${style.logo}`} />
